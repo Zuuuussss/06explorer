348c232014e57fa7ddfcac458cb79a76d16c77c9
Zuuuussss <ti.tift@hotmail.com>
2024-03-06 02:57:40 -0300

    handlereset fail

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+// variaveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-// função callback
+//eventos
+
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handResetClick)
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Enter") {
+    handResetClick()
+  }
+})
+
+// funções
+function handResetClick() {
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+  xAttempts = 1
+}
+
 function handleTryClick(event) {
   event.preventDefault()
 
@@ -14,24 +32,9 @@ function handleTryClick(event) {
     screen1.classList.add("hide")
     screen2.classList.remove("hide")
 
-    document.querySelector(
-      ".screen2 h2"
-    ).innerText = `acertou em ${xAttempts} tentativas`
+    screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
   }
 
   inputNumber.value = ""
   xAttempts++
 }
-
-// Eventos
-
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-btnTry.addEventListener("click", handleTryClick)
-btnReset.addEventListener("click", function () {})
-
-screen1.classList.remove("hide")
-screen2.classList.add("hide")
-
-xAttempts = 1
